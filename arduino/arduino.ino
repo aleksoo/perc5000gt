40ad17621382da08a9bf64343b3b35ea33d39ac8
Alekso <a.walecki95@gmail.com>
2021-04-01 19:58:48 +0200

    Encoder plus fixes

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,45 +1,40 @@
 #include <Keypad.h>
 #include <Button.h>
+#include <Encoder.h>
 
 #include "src/Grid.h"
 
-// Potentiometer
+  // Potentiometer
 int potPin = 21;
 int potVal = 0;
 
-// Sequencing grid
+  // Sequencing grid
 Grid grid;
 int lenght = 16; // TBD: lenght of pattern
 int stepCounter = 0;
 
-// Keypad variables
+  // Keypad variables
 const byte KEYPAD_ROWS = 4;
 const byte KEYPAD_COLS = 4;
 
 char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
-  // {'1','2','3','4'},
-  // {'5','6','7','8'},
-  // {'a','b','c','d'},
-  // {'e','f','g','h'}
-  // {0, 1, 2, 3},
-  // {4, 5, 6, 7,},
-  // {8, 9, 10, 11},
-  // {12, 13, 14, 15}
   {'a','b','c','d'},
   {'e','f','g','h'},
   {'i','j','k','l'},
-  {'m','n','o','p'}
-  
+  {'m','n','o','p'}  
 };
 
-byte rowPins[KEYPAD_ROWS] = {2, 3, 4, 5};
-byte colPins[KEYPAD_COLS] = {6, 7, 8, 9};
+byte rowPins[KEYPAD_ROWS] = {5, 4, 3, 2};
+byte colPins[KEYPAD_COLS] = {9, 8, 7, 6};
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS );
 
 
-// Additional variables
-//int playButton = 10;
+  // Encoder
+Encoder encoder(14, 16);
+long oldPosition = -999;
+
+  // Play/Pause button variable
 Button playPauseButton(10);
 bool playPauseButtonState = true;
 
@@ -61,39 +56,39 @@ void setup() {
 }
 
 void loop() {
-  
-
-  int key = keypad.getKey();
-  if (key != NO_KEY){
-    // Serial.print("Key pressed: ");
-    // Serial.println(key - 97);
-    grid.gridFlipState(1, key - 97);
-    // Serial.println("Flipped state");
+  long newPosition = encoder.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
   }
-
   
-  int bpmVal = map(analogRead(potPin), 0, 1023, 60, 180); 
-  int bpmTime = 60000 / bpmVal / 4;
-
-  if (playPauseButton.released()) {
-    // Serial.print("Button state: ");
-    playPauseButtonState = !playPauseButtonState;
-    // Serial.println(playPauseButtonState);
-    if (playPauseButtonState == true) stepCounter = 0;
-  }
-  // Serial.println(playPauseButtonState);
-  
-  // Send msg to RPI after delay time between steps
-  if ( (millis() - timeLastAction > bpmTime) && playPauseButtonState) {
-    // grid.getGrid();
-    String msgToRpi = grid.getGridState(stepCounter);
-    timeLastAction = millis();
-    if(msgToRpi.length()) {
-      // grid.getGrid();
-      Serial.println(msgToRpi); // Sending msg to RPI
-    }  
-
-    stepCounter++;
-    if (stepCounter > 15) stepCounter = 0;
-  }  
+//  // BPM setting
+//  int bpmVal = map(analogRead(potPin), 0, 1023, 60, 180); 
+//  int bpmTime = 60000 / bpmVal / 4;
+//  
+//  // Step sequencer editing
+//  int key = keypad.getKey();
+//  if (key != NO_KEY){
+//    grid.gridFlipState(1, key - 97);
+//  }
+//  
+//  // Play/Pause button
+//  if (playPauseButton.released()) {
+//    playPauseButtonState = !playPauseButtonState;
+//    if (playPauseButtonState == true) stepCounter = 0;
+//  }
+//  
+//  // Send msg to RPI after delay time between steps
+//  if ( (millis() - timeLastAction > bpmTime) && playPauseButtonState) {
+//    String msgToRpi = grid.getGridState(stepCounter);
+//    timeLastAction = millis();
+//    if(msgToRpi.length()) {
+//      // grid.getGrid();
+//      Serial.println(msgToRpi); // Sending msg to RPI
+//    }  
+//
+//    // Move to another step
+//    stepCounter++;
+//    if (stepCounter > 15) stepCounter = 0;
+//  }  
 }
